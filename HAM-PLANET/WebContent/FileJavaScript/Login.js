fbafc7c1dd4d306d0310fdc31e94a1aaf9b7ec9c
catello-vitiello <catellovitiello00@gmail.com>
2023-05-15 20:02:40 +0200

    aggiunto controllo sulla formattazzione corretta dell'email con cambio anche di icona in javascript a seconda dell'input inserito

--- HAM-PLANET/WebContent/FileJavaScript/Login.js
+++ HAM-PLANET/WebContent/FileJavaScript/Login.js
@@ -1,48 +1,81 @@
 
-		/** FUNZIONE PER CAMBIARE ICONA EMAIL */
-		var num = 0;
-		var mom = false;
-		function changeIcon(numero){
-		    var box = document.getElementById('email');
-		    var input = document.getElementById('email').value;
-		    var letter = document.getElementById('img_email');
-
-		    num = numero;
-
-		    /**##################################### ZERO ################################## */
-		    if (num == 0){
-		        letter.src = "./Icon/email_WHITE.png";
-		    }
-
-		    /**##################################### UNO ################################## */
-		    else if (num == 1){
-		        if (input === '' && mom == false){
-		            letter.src = "./Icon/email_BLACK.png";
-		        }
-		        else{
-		            letter.src = "./Icon/email_WHITE.png";
-		        }
-		    }
-		    
-
-		    /**##################################### DUE ################################## */
-		    else if (num == 2){
-		        letter.src = "./Icon/email_WHITE.png"
-		        mom = true;
-		    }
 
 
-		    /**##################################### TRE ################################## */  
-		    else if (num == 3){
-		        if(input === ''){
-		            letter.src = "./Icon/email_BLACK.png";
-		        }
-		        else{
-		            letter.src = "./Icon/email_WHITE.png"
-		        }
-		        mom = false;
-		    }
-		}
+
+/** FUNZIONE PER CAMBIARE ICONA EMAIL */
+var num = 0;
+var email_bool = false;
+function changeIcon(numero){
+    var email = document.getElementById('email').value;
+    var letter = document.getElementById('img_email');
+
+    num = numero;
+
+    /**##################################### ZERO ################################## */
+    if (num == 0){
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+            letter.src = "./Icon/email_accept_GREEN.png";   
+        }else if (email === '' && email_bool == false){
+            letter.src = "./Icon/email_default_WHITE.png";
+        }else if (email !== '' && !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+            letter.src = "./Icon/email_reject_RED.png";
+        }
+
+        
+    }
+
+    /**##################################### UNO ################################## */
+    else if (num == 1){
+        if (email === '' && email_bool == false){
+            letter.src = "./Icon/email_default_BLACK.png";
+        }
+        else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            letter.src = "./Icon/email_accept_GREEN.png";
+        }else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)|| email ==='') {
+            letter.src = "./Icon/email_reject_RED.png";
+        }
+    }
+    
+
+    /**##################################### DUE ################################## */
+    else if (num == 2){
+        letter.src = "./Icon/email_default_WHITE.png";
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+            letter.src = "./Icon/email_accept_GREEN.png";
+        }else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) && email !== ''){
+            letter.src = "./Icon/email_reject_RED.png";
+        }else if (email === ''){
+            letter.src = "./Icon/email_reject_RED.png"
+        }else{
+            letter.src = "./Icon/email_default_WHITE.png";
+        }
+        email_bool = true;
+    }
+
+
+    /**##################################### TRE ################################## */  
+    else if (num == 3){
+        if(email === ''){
+            letter.src = "./Icon/email_default_BLACK.png";
+        }
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+            letter.src = "./Icon/email_accept_GREEN.png";
+        }else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) && email !== ''){
+            letter.src = "./Icon/email_reject_RED.png";
+        }
+        email_bool = false;
+    }
+
+    /**##################################### QUATTRO ################################## */  
+    else if (num == 4){
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+            letter.src = "./Icon/email_accept_GREEN.png";
+        }else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) || email === ''){
+            letter.src = "./Icon/email_reject_RED.png";
+        }
+    }
+
+}
 		 
 		 
 		 
@@ -52,7 +85,7 @@
 		/** FUNZIONE MOSTRA/NASCONDI PASSWORD */
        function showPwd(){
            var input = document.getElementById('pwd');
-           var eyes = document.getElementById('img');
+           var eyes = document.getElementById('img_pass');
 
            if(input.value === ''){
                if(input.type == "password"){
@@ -103,7 +136,7 @@
        function changeColor(numero){
            var box = document.getElementById('pwd');
            var input = document.getElementById('pwd').value;
-           var eyes = document.getElementById('img');
+           var eyes = document.getElementById('img_pass');
 
            num = numero;
 
--- HAM-PLANET/WebContent/Login.jsp
+++ HAM-PLANET/WebContent/Login.jsp
@@ -6,8 +6,8 @@
 <html>
     <head>
         <title>Gestisci il tuo ID HamPlanet</title>
-        <link rel="stylesheet" href="./FileCSS/Login.css">
-		<script type="text/javascript" src="./FileJavaScript/Login.js"></script>
+        <link rel="stylesheet" href="./FileCSS/Login.css?ts=<?=time()?>&quot">
+		<script type="text/javascript" src="./FileJavaScript/Login.js?ts=<?=time()?>&quot"></script>
         <link rel="shortcut icon" href=""> <!-- AGGIUNGERE PERCORSO LOGO PICCOLO -->
     </head>
 
@@ -17,9 +17,9 @@
             <form action="ClienteControl" method="post"> 
 
                 <!--LOGO-->
-                <!--   <img id="logo" src="" alt="Ham Planet" height="130x">  aggiungere logo black -->
-            
-
+                <img id="logo" src="./Icon/logo_HamPlanet_BLACK.png" alt="Ham Planet" height="130x">
+                
+                
                 <!--TITOLO-->
                 <h2 id="titolo">Accedi</h2>
                 <!--INFO-->
@@ -32,6 +32,7 @@
                                       onpointerleave="changeIcon(1)"
                                       onclick="changeIcon(2)"
                                       onblur="changeIcon(3)"
+                                      oninput="changeIcon(4)"
                                       type="text"  name="email" required>
                     <div class="testo">
                         <label class="uno">E</label>
@@ -40,7 +41,7 @@
                         <label class="quattro">i</label>
                         <label class="cinque">l</label>
                     </div>
-                    <img id="img_email" src="./Icon/email_BLACK.png" alt="show">
+                    <img id="img_email" src="./Icon/email_default_BLACK.png" alt="show" height="50px">
                 </div>
 
                 <!--PASSWORD-->
@@ -61,7 +62,7 @@
                         <label class="sette">r</label>
                         <label class="otto">d</label>
                     </div>
-                    <img id="img" src="./Icon/eyes_show_BLACK.png" alt="show" onclick="showPwd()">
+                    <img id="img_pass" src="./Icon/eyes_show_BLACK.png" alt="show" height="30px" onclick="showPwd()">
                 </div>
 
                 <!--PASSWORD DIMENTICATA-->
