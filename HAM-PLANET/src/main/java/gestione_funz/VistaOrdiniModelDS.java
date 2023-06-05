cb50c3967bc8abde5d5fc9a48cdcf8c78d6c891d
MarioNappi <marionappi@outlook.it>
2023-06-05 09:03:38 +0200

    Aggiunto controllo per vedere se l'immagine è vuota in ProdottoModelDS,aggiunto metodo per l'inserimento del prodotto senza immagine nella servlet ProdottoConrol.

--- HAM-PLANET/src/main/java/gestione_funz/VistaOrdiniModelDS.java
+++ HAM-PLANET/src/main/java/gestione_funz/VistaOrdiniModelDS.java
@@ -12,17 +12,16 @@ import javax.sql.DataSource;
 public class VistaOrdiniModelDS implements VistaOrdiniModel<VistaOrdiniBean> {
 
 	private DataSource ds = null;
-	
+
 	/********************************************************/
-	/*				COSTRUTTORE CON DATASOURCE				*/
+	/* 			COSTRUTTORE CON DATASOURCE 					*/
 	/********************************************************/
 	public VistaOrdiniModelDS(DataSource ds) {
 		this.ds = ds;
 	}
-	
-	@Override
+
 	public Collection<VistaOrdiniBean> getOrdiniByemail(String email) throws SQLException {
-		
+
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		String sql = "SELECT ordine.data_acquisto, composto.ian_prodotto, "
@@ -30,41 +29,41 @@ public class VistaOrdiniModelDS implements VistaOrdiniModel<VistaOrdiniBean> {
 				+ "FROM cliente, ordine, composto, prodotto "
 				+ "WHERE ordine.email = cliente.e_mail AND composto.ian_prodotto = prodotto.IAN "
 				+ "AND composto.id_ordine = ordine.id and cliente.e_mail = ?";
-		
+
 		Collection<VistaOrdiniBean> ordini = new LinkedList<VistaOrdiniBean>();
-		
+
 		try {
-			
+
 			connection = ds.getConnection();
 			preparedStatement = connection.prepareStatement(sql);
-			
+
 			preparedStatement.setString(1, email);
 			utils.UtilityClass.print("Vista ordini " + preparedStatement.toString());
-			
+
 			ResultSet rs = preparedStatement.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				VistaOrdiniBean bean = new VistaOrdiniBean();
-				
+
 				bean.setData(rs.getDate("data_acquisto"));
-				//System.out.println(bean.getData().toString());
+				// System.out.println(bean.getData().toString());
 				bean.setIan(rs.getInt("ian_prodotto"));
-				//System.out.println(bean.getIan());
+				// System.out.println(bean.getIan());
 				bean.setNomeProdotto(rs.getString("nomeProdotto"));
-				//System.out.println(bean.getNomeProdotto());
+				// System.out.println(bean.getNomeProdotto());
 				bean.setDescrizione(rs.getString("descrizione"));
-				//System.out.println(bean.getDescrizione());
+				// System.out.println(bean.getDescrizione());
 				bean.setPrezzo(rs.getDouble("prezzo"));
-				//System.out.println(bean.getPrezzo());
+				// System.out.println(bean.getPrezzo());
 				bean.setPeso(rs.getDouble("peso"));
-				//bean.setQuantity(rs.getInt("quantity"));
-				
+				// bean.setQuantity(rs.getInt("quantity"));
+
 				ordini.add(bean);
 			}
-			
-		}finally {
-			if(preparedStatement != null)
+
+		} finally {
+			if (preparedStatement != null)
 				preparedStatement.close();
-			if(connection != null)
+			if (connection != null)
 				connection.close();
 		}
 		return ordini;
